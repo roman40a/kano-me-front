eda57b6a689d5830319b5f0df566ef5c98f6a9c3
rsoroka <rsoroa@luxoft.com>
2019-11-19 16:16:48 +0300

    Add header help item component

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -7,28 +7,52 @@ import { Cart } from '../cart/cart.component';
 import { PointerIcon } from '../icons/pointer/pointer';
 import { Item } from './sub-components/item/item.component';
 import { ProductsItem } from './sub-components/products-item/products-item.component';
+import { HelpItem } from './sub-components/help-item/help-item.component';
 
 export type Props = {
     data?: any;
 }
 
 type State = {
-    isProductHovered: boolean;
     isProjectsOpened: boolean;
+    isHelpOpened: boolean;
 }
+
+const HELP_ITEMS = [
+    {
+        label: 'Contact Us',
+        href: '/',
+    },
+    {
+        label: 'Help Center',
+        href: '/',
+    },
+    {
+        label: 'Community',
+        href: '/',
+    },
+    {
+        label: 'Career',
+        href: '/',
+    },
+];
 
 export class Header extends React.PureComponent<Props, State> {
     readonly state: State = {
-        isProductHovered: false,
         isProjectsOpened: false,
+        isHelpOpened: false,
     };
 
     handleProductOpen = () => {
         this.setState(state => ({ isProjectsOpened: !state.isProjectsOpened }));
     };
 
+    handleHelpOpen = () => {
+        this.setState(state => ({ isHelpOpened: !state.isHelpOpened }));
+    };
+
     render() {
-        const {isProjectsOpened} = this.state;
+        const {isProjectsOpened, isHelpOpened} = this.state;
 
         const showCaseClassName = cn(css.showCase, {
             [css.showCase__open]: isProjectsOpened,
@@ -54,6 +78,7 @@ export class Header extends React.PureComponent<Props, State> {
                             <div className={css.secondary}>
                                 <Item label={'Blog'} href={'/'} />
                                 <Item label={'Story Locator'} href={'/'} />
+                                <HelpItem label={'Help'} isActive={isHelpOpened} items={HELP_ITEMS} onClick={this.handleHelpOpen} />
                             </div>
                         </div>
                         <div className={css.auxiliary}>
---
+++ src/components/header/sub-components/help-item/help-item.component.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import cn from 'classnames';
+import { Item } from '../item/item.component';
+import { ProductsItem } from '../products-item/products-item.component';
+
+import css from './help-item.module.css';
+
+type ItemContent = {
+    label: string;
+    href: string;
+}
+
+export type Props = {
+    label: string;
+    isActive: boolean;
+    items: ItemContent[];
+    onClick(): void;
+}
+
+export class HelpItem extends React.PureComponent<Props> {
+    render() {
+        const {label, isActive, onClick, items} = this.props;
+
+        const contentClassName = cn(css.content, {
+            [css.content__opened]: isActive,
+        });
+
+        return (
+            <div className={css.container}>
+                <ProductsItem label={label} isActive={isActive} onClick={onClick} />
+                <div className={contentClassName}>
+                    {items.map(item => (
+                        <div className={css.itemContainer}>
+                            <Item label={item.label} href={item.href} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
