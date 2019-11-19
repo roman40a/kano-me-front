2b03f7c897bba5e3aa2fa568ee58b33b13469535
rsoroka <rsoroa@luxoft.com>
2019-11-19 22:23:54 +0300

    Updates

--- precache-manifest.997f8882681f7726b09ad329ab372406.js
+++ precache-manifest.997f8882681f7726b09ad329ab372406.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bd0eeb63317558f8a64ec3ba32f7c004",
+    "revision": "bcedd4d5f0b2f8b70d0a03d20b19700c",
     "url": "/kano-me-front/index.html"
   },
   {
-    "revision": "6cac2db0acb1dd4b4028",
-    "url": "/kano-me-front/static/css/main.a9a3f64c.chunk.css"
+    "revision": "e09435c2435b311d7e9a",
+    "url": "/kano-me-front/static/css/main.8bc0ddb7.chunk.css"
   },
   {
-    "revision": "d4f6fa2f5040239cf33a",
-    "url": "/kano-me-front/static/js/2.88f0ff6d.chunk.js"
+    "revision": "fdd2cddce35e1f917870",
+    "url": "/kano-me-front/static/js/2.98c6af1c.chunk.js"
   },
   {
-    "revision": "6cac2db0acb1dd4b4028",
-    "url": "/kano-me-front/static/js/main.139e1cc0.chunk.js"
+    "revision": "e09435c2435b311d7e9a",
+    "url": "/kano-me-front/static/js/main.aea92c75.chunk.js"
   },
   {
     "revision": "891995ebb24b81770e96",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kano-me-front/precache-manifest.03758dd18075f99def088a17e1a88da6.js"
+  "/kano-me-front/precache-manifest.997f8882681f7726b09ad329ab372406.js"
 );
 
 self.addEventListener('message', (event) => {
